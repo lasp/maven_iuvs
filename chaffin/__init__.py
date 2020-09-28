ed6b3531d17f9cc9318b1e1d6aec07ab9a82b422
Mike <michael.s.chaffin@gmail.com>
2020-09-28 14:28:59 -0600

    initial commit of H Lyman alpha orbit overview code

--- chaffin/__init__.py
+++ chaffin/__init__.py
@@ -8,7 +8,6 @@ from .file_operations import latestfiles
 from .file_operations import getfilenames
 
 from .graphics import getcmap
-from .graphics import plotdetector
 
 from .integration import fit_line
 from .integration import get_line_calibration
@@ -18,3 +17,5 @@ from .spice import load_iuvs_spice
 from .time import Ls
 from .time import Ls_to_et
 from .time import et_to_utc
+
+from .quicklooks.quicklook import H_corona_quicklook
