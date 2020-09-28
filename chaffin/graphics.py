86ea74919b1390b23c487f8d14aeec07ab798695
Mike <michael.s.chaffin@gmail.com>
2020-09-28 14:28:15 -0600

    added maven orbit image code

--- chaffin/graphics.py
+++ chaffin/graphics.py
@@ -294,3 +294,390 @@ class line_fit_plot:
         cax = self.fig.add_axes((ax_pos.x1+cax_margin,ax_pos.y0,cax_width,ax_pos.height))
         self.fig.colorbar(pcm, cax=cax)
 
+    
+def fig2rgb_array(fig):
+    fig.canvas.draw()
+    buf = fig.canvas.tostring_rgb()
+    ncols, nrows = fig.canvas.get_width_height()
+    return np.frombuffer(buf, dtype=np.uint8).reshape(nrows, ncols, 3)
+    
+def maven_orbit_image(time, 
+                      camera_pos=[1,0,0], camera_up=[0,0,1], extent=3, 
+                      parallel_projection=True, view_from_orbit_normal=False, view_from_periapsis=False,
+                      show_maven=False, show_orbit=True, label_poles=None,
+                      show=True, transparent_background=False, background_color=(0,0,0)):
+    from mayavi import mlab
+    mlab.options.offscreen = True
+    from tvtk.api import tvtk # python wrappers for the C++ vtk ecosystem
+    from tvtk.common import configure_input_data
+    import spiceypy as spice
+    
+    orbitcolor=np.array([222,45,38])/255 # a nice red
+    orbitcolor=tuple(orbitcolor)
+    
+    # create a figure window (and scene)
+    mlab_pix = 1000
+    mfig = mlab.figure(size=(mlab_pix, mlab_pix),bgcolor=background_color)
+
+    mfig.scene.disable_render = True
+
+    # load and map the texture
+    import os
+    from .paths import anc_dir
+    image_file = os.path.join(anc_dir,'marssurface_2.jpg')
+    img = tvtk.JPEGReader()
+    img.file_name = image_file
+    texture = tvtk.Texture(input_connection=img.output_port, interpolate=1)
+
+    # use a TexturedSphereSource, a.k.a. getting our hands dirty
+    R = 1
+    Nrad = 180
+    sphere = tvtk.TexturedSphereSource(radius=R, theta_resolution=Nrad,
+                                       phi_resolution=Nrad)
+    sphere_mapper = tvtk.PolyDataMapper(input_connection=sphere.output_port)
+    mars = tvtk.Actor(mapper=sphere_mapper, texture=texture)
+    mars.property.ambient=0.2 # so the nightside is slightly visible
+    mars.property.specular=0.15 #make it shinier near dayside
+    
+    #now apply the rotation matrix
+    #tvtk only thinks about rotations with Euler angles, so we need to use a SPICE routine to get these
+    myet=spice.str2et(time)
+    rmat=spice.pxform('IAU_MARS','MAVEN_MSO',myet)
+    trmat=spice.pxform('MAVEN_MSO','IAU_MARS',myet)#we need to use transpose because m2eul assumes the matrix defines a coordinate system rotation, the inverse of the matrix to rotate vectors
+    rangles=np.rad2deg(spice.m2eul(trmat,2,1,3))#2,1,3 because vtk performs rotations in the order z,x,y and SPICE wants these in REVERSE order
+    mars.orientation=rangles[[1,0,2]]#orientation must be specified as x,y,z rotations in that order even though they are applied in the order above
+    #OK, that was hard, but now we're good!
+
+    mfig.scene.add_actor(mars)
+
+    #make a lat/lon grid
+    line_t = np.linspace(0,2*np.pi,100)
+    line_r = 1.0
+    longrid=np.arange(0,360,30)
+    grid_linewidth=0.25*mlab_pix/1000
+    line_x=[]
+    line_y=[]
+    line_z=[]
+    line_o=[]
+    for lon in longrid:
+        line_x.append(line_r*np.cos(np.deg2rad(lon))*np.cos(line_t))
+        line_x.append([0])
+        line_y.append(line_r*np.sin(np.deg2rad(lon))*np.cos(line_t))
+        line_y.append([0])
+        line_z.append(line_r*np.sin(line_t))
+        line_z.append([0])
+        line_o.append(np.ones_like(line_t))
+        line_o.append([0])
+
+    latgrid=np.arange(-90,90,30)[1:]
+    for lat in latgrid:
+        line_x.append(line_r*np.cos(np.deg2rad(lat))*np.cos(line_t))
+        line_x.append([0])
+        line_y.append(line_r*np.cos(np.deg2rad(lat))*np.sin(line_t))
+        line_y.append([0])
+        line_z.append(line_r*np.sin(np.deg2rad(lat))*np.ones_like(line_t))
+        line_z.append([0])
+        line_o.append(np.ones_like(line_t))
+        line_o.append([0])
+
+    line_x=np.concatenate(line_x)
+    line_y=np.concatenate(line_y)
+    line_z=np.concatenate(line_z)
+    line_o=np.concatenate(line_o)
+    line_x,line_y,line_z=np.transpose(np.array([np.matmul(rmat,[x,y,z]) for x,y,z in zip(line_x,line_y,line_z)]))
+    mlab.plot3d(line_x,line_y,line_z,line_o,transparent=True,color=(0,0,0),tube_radius=None,line_width=grid_linewidth)
+
+
+    #add the spacecraft orbit
+
+    #for the given time, we determine the orbit length
+    maven_state=spice.spkezr('MAVEN',myet,'MAVEN_MME_2000','NONE','MARS')[0]
+    marsmu=spice.bodvrd('MARS','GM',1)[1][0]
+    maven_elements=spice.oscltx(maven_state,myet,marsmu)
+    orbit_period=1.001*maven_elements[-1]
+
+    #get the times corresponding to the half-orbit ahead and behind
+    orbit_subdivisions=2000
+    etlist=myet-orbit_period/2+orbit_period*np.linspace(0,1,num=orbit_subdivisions)
+
+    #get the position of the orbit in MSO
+    statelist=spice.spkezr('MAVEN',etlist,'MAVEN_MSO','NONE','MARS')[0]
+    statelist=np.append(statelist,[statelist[0]],axis=0)
+    poslist=np.transpose(statelist)[:3]/3395 #scale to Mars radius = 1
+
+    #plot the orbit
+    maven_x,maven_y,maven_z=poslist
+    if show_orbit:
+        mlab.plot3d(maven_x,maven_y,maven_z,color=orbitcolor,tube_radius=None,line_width=3*mlab_pix/1000)
+
+    #add a dot indicating the location of the Sun
+    #this only makes sense with a perspective transform... with orthographic coordinates we're always too far away
+    if not parallel_projection:
+        sun_distance = 10
+        sun_sphere = tvtk.SphereSource(center=(sun_distance,0,0),radius=1*np.pi/180*sun_distance, theta_resolution=Nrad, phi_resolution=Nrad)
+        sun_sphere_mapper = tvtk.PolyDataMapper(input_connection=sun_sphere.output_port)
+        sun_sphere = tvtk.Actor(mapper=sun_sphere_mapper)
+        sun_sphere.property.ambient=1.0
+        sun_sphere.property.lighting=False
+        #mfig.scene.add_actor(sun_sphere)
+
+        #put a line along the x-axis towards the sun
+        sunline_x=np.arange(0,5000,1)
+        #mlab.plot3d(sunline_x,0*sunline_x,0*sunline_x,color=(1.0,1.0,1.0),tube_radius=None,line_width=6)
+
+
+    #define the coordinate system with respect to the camera
+    if view_from_periapsis:
+        #now we need to get the position of apoapsis and the orbit normal
+        rlist=[np.linalg.norm(p) for p in np.transpose(poslist)]
+        apoidx=np.argmax(rlist)
+        apostate=spice.spkezr('MAVEN',etlist[apoidx],'MAVEN_MSO','NONE','MARS')[0]
+        camera_pos=-apostate[:3]
+        camera_pos=5*camera_pos/np.linalg.norm(camera_pos)
+        camera_up=np.cross(apostate[:3],apostate[-3:])
+        camera_up=camera_up/np.linalg.norm(camera_up)
+        parallel_projection=True
+
+    if view_from_orbit_normal:
+        #now we need to get the position of apoapsis and the orbit normal
+        rlist=[np.linalg.norm(p) for p in np.transpose(poslist)]
+        apoidx=np.argmax(rlist)
+        apostate=spice.spkezr('MAVEN',etlist[apoidx],'MAVEN_MSO','NONE','MARS')[0]
+        camera_up=apostate[:3]
+        camera_up=camera_up/np.linalg.norm(camera_up)
+        camera_pos=np.cross(apostate[:3],apostate[-3:])
+        camera_pos=5*camera_pos/np.linalg.norm(camera_pos)
+        parallel_projection=True
+
+    camera_pos_norm = camera_pos/np.linalg.norm(camera_pos)
+    camera_up = camera_up-camera_pos_norm*np.dot(camera_pos_norm,camera_up)
+    camera_up = camera_up/np.linalg.norm(camera_up)
+    camera_right = np.cross(-camera_pos_norm,camera_up)
+
+
+    #set location of camera and orthogonal projection
+    camera=mlab.gcf().scene.camera
+    if parallel_projection:
+        camera_pos=5*camera_pos_norm
+        camera.parallel_projection = True
+        camera.parallel_scale=extent # half box size
+    else:
+        camera.parallel_projection = False
+        camera.view_angle=50
+
+    camera.position = np.array(camera_pos)
+    camera.focal_point = (0,0,0)
+    camera.view_up = camera_up
+    camera.clipping_range = (0.01,5000)
+
+
+    #the only way to set a light in mayavi/vtk is with respect to the camera position
+    #default lights are uniform and don't fall off with distance, which is what we want
+    mfig.scene.light_manager.light_mode = "vtk"
+    sun = mfig.scene.light_manager.lights[0]
+    sun.activate = True
+    sun_vec=(1,0,0)
+
+    #get the elevation/azimuth coordinates
+    #elevation is [-90/+90] +90 is from the direction of camera_up
+    #azimuth is [-180/+180] +90 is to the right of the image; +/-180 is behind, pointing at the camera -90 is to the left
+
+    #we need to put the sun in scene coordinates
+    sun_scene = np.matmul([camera_right, camera_up, camera_pos_norm],sun_vec)
+    
+    #elevation is the angle is latitude measured wrt the y-axis of the scene
+    sun_elevation = np.rad2deg(np.arcsin(np.dot(sun_scene,[0,1,0])))
+    #azimuth is the angle in the x-z plane clockwise from the z-axis
+    sun_azimuth = np.rad2deg(np.arctan2(sun_scene[0],sun_scene[2]))
+    
+    sun.azimuth = sun_azimuth
+    sun.elevation = sun_elevation
+    
+    sun.intensity = 1.0-mars.property.ambient
+
+    mfig.scene.disable_render = False
+    #mfig.scene.anti_aliasing_frames = 0 # can uncomment to make rendering faster and uglier
+    mlab.show()
+    mode = 'rgba' if transparent_background else 'rgb'
+    img=mlab.screenshot(mode=mode,antialiased=True)
+    mlab.close()
+
+    fig,ax=plt.subplots(1,1,dpi=400*mlab_pix/1000,figsize=(2.5,2.5))
+    ax.imshow(img)
+
+    #put an arrow along the orbit direction
+    arrow_width=5
+    arrow_length=1.5*arrow_width
+    # put it at the closest point on the orbit to the viewer
+    if show_orbit:
+        arrowidx=np.argmax([np.dot(camera_pos/np.linalg.norm(camera_pos),p) for p in np.transpose(poslist)])
+        arrowetlist=etlist[arrowidx]+5*60*np.array([0,1])
+        arrowstatelist=spice.spkezr('MAVEN',arrowetlist,'MAVEN_MSO','NONE','MARS')[0]
+        arrowdir=arrowstatelist[1][:3]-arrowstatelist[0][:3]
+        arrowdirproj=[np.dot(camera_right,arrowdir),np.dot(camera_up,arrowdir)]
+        arrowdirproj=arrowdirproj/np.linalg.norm(arrowdirproj)
+
+        arrowloc=np.transpose(poslist)[arrowidx]
+        arrowlocproj=np.array([np.dot(camera_right,arrowloc),np.dot(camera_up,arrowloc)])
+        arrowlocdisp=(arrowlocproj+extent)/extent/2
+        arrow = ax.annotate('',
+	                xytext=arrowlocdisp-0.05*arrowdirproj,
+	                xy=arrowlocdisp+0.05*arrowdirproj,
+	                xycoords='axes fraction',
+	                textcoords='axes fraction',
+	                arrowprops=dict(facecolor=orbitcolor,edgecolor='none',width=0,headwidth=arrow_width,headlength=arrow_length))
+
+    if view_from_periapsis: 
+        #we need to redraw the arrow so it's always in the same place
+        if show_orbit:
+            arrow.remove()
+            arrowidx=np.argmax([np.dot((camera_right+camera_pos/np.linalg.norm(camera_pos))/np.linalg.norm(camera_right+camera_pos/np.linalg.norm(camera_pos)),p) for p in np.transpose(poslist)])
+            arrowetlist=etlist[arrowidx]+5*60*np.array([0,1])
+            arrowstatelist=spice.spkezr('MAVEN',arrowetlist,'MAVEN_MSO','NONE','MARS')[0]
+            arrowdir=arrowstatelist[1][:3]-arrowstatelist[0][:3]
+            arrowdirproj=[np.dot(camera_right,arrowdir),np.dot(camera_up,arrowdir)]
+            arrowdirproj=arrowdirproj/np.linalg.norm(arrowdirproj)
+
+            arrowloc=np.transpose(poslist)[arrowidx]
+            arrowlocproj=np.array([np.dot(camera_right,arrowloc),np.dot(camera_up,arrowloc)])
+            arrowlocdisp=(arrowlocproj+extent)/extent/2
+            arrow = ax.annotate('',
+		                xytext=arrowlocdisp-0.05*arrowdirproj,
+		                xy=arrowlocdisp+0.05*arrowdirproj,
+		                xycoords='axes fraction',
+		                textcoords='axes fraction',
+		                arrowprops=dict(facecolor=orbitcolor,edgecolor='none',width=0,headwidth=arrow_width,headlength=arrow_length))
+        label_poles = True
+
+
+    if view_from_orbit_normal: 
+        #we need to redraw the arrow so it's always in the same place
+        if show_orbit:
+            arrow.remove()
+            arrowidx=np.argmax([np.dot((camera_right-camera_up)/np.linalg.norm(camera_right-camera_up),p) for p in np.transpose(poslist)])
+            arrowetlist=etlist[arrowidx]+5*60*np.array([0,1])
+            arrowstatelist=spice.spkezr('MAVEN',arrowetlist,'MAVEN_MSO','NONE','MARS')[0]
+            arrowdir=arrowstatelist[1][:3]-arrowstatelist[0][:3]
+            arrowdirproj=[np.dot(camera_right,arrowdir),np.dot(camera_up,arrowdir)]
+            arrowdirproj=arrowdirproj/np.linalg.norm(arrowdirproj)
+
+            arrowloc=np.transpose(poslist)[arrowidx]
+            arrowlocproj=np.array([np.dot(camera_right,arrowloc),np.dot(camera_up,arrowloc)])
+            arrowlocdisp=(arrowlocproj+extent)/extent/2
+            arrow = ax.annotate('',
+		                xytext=arrowlocdisp-0.05*arrowdirproj,
+		                xy=arrowlocdisp+0.05*arrowdirproj,
+		                xycoords='axes fraction',
+		                textcoords='axes fraction',
+		                arrowprops=dict(facecolor=orbitcolor,edgecolor='none',width=0,headwidth=arrow_width,headlength=arrow_length))
+        label_poles = True
+	
+    if label_poles==None:
+        label_poles=False
+
+    if label_poles:
+        import matplotlib.patheffects as path_effects
+        
+        #label the north and south pole
+        npolepos=np.matmul(rmat,[0,0,1])
+        npoleposproj=np.array([np.dot(camera_right,npolepos),np.dot(camera_up,npolepos)])
+        npoleposdisp=(npoleposproj+extent)/extent/2
+        npolevis=not (np.linalg.norm([npoleposproj])<1 and np.dot(camera_pos,npolepos)<0)
+        if npolevis:
+            npolelabel = ax.text(*npoleposdisp, 'N',transform=ax.transAxes,color='#888888',ha='center',va='center',size=4,zorder=1)
+            npolelabel.set_path_effects([path_effects.withStroke(linewidth=0.75, foreground='k')])
+
+
+        spolepos=np.matmul(rmat,[0,0,-1])
+        spoleposproj=np.array([np.dot(camera_right,spolepos),np.dot(camera_up,spolepos)])
+        spoleposdisp=(spoleposproj+extent)/extent/2
+        spolevis=not (np.linalg.norm([spoleposproj])<1 and np.dot(camera_pos,spolepos)<0)
+        if spolevis:
+            spolelabel = ax.text(*spoleposdisp, 'S',transform=ax.transAxes,color='#888888',ha='center',va='center',size=4,zorder=1)
+            spolelabel.set_path_effects([path_effects.withStroke(linewidth=0.75, foreground='k')])
+
+    #add a mark for periapsis and apoapsis
+    if show_orbit:
+        #find periapsis/apoapsis
+        rlist=[np.linalg.norm(p) for p in np.transpose(poslist)]
+        periidx=np.argmin(rlist)
+        peripos=np.transpose(poslist)[periidx]
+        periposproj=np.array([np.dot(camera_right,peripos),np.dot(camera_up,peripos)])
+        periposdisp=(periposproj+extent)/extent/2
+        perivis=not (np.linalg.norm([periposproj])<1 and np.dot(camera_pos,peripos)<0)
+
+        apoidx=np.argmax(rlist)
+        apopos=np.transpose(poslist)[apoidx]
+        apoposproj=np.array([np.dot(camera_right,apopos),np.dot(camera_up,apopos)])
+        apoposdisp=(apoposproj+extent)/extent/2
+        apovis=not (np.linalg.norm([apoposproj])<1 and np.dot(camera_pos,apopos)<0)
+
+        #periapsis and apoapsis
+        import matplotlib.patches as mpatches
+        if perivis:
+            peri = mpatches.CirclePolygon(periposdisp, 0.015, resolution=4, transform=ax.transAxes, fc=orbitcolor, lw=0,zorder=10)
+            ax.add_patch(peri)
+            ax.text(*periposdisp, 'P',transform=ax.transAxes,color='k',ha='center',va='center_baseline',size=4,zorder=10) 
+
+        if apovis:
+            apo = mpatches.CirclePolygon(apoposdisp, 0.015, resolution=4, transform=ax.transAxes, fc=orbitcolor,lw=0,zorder=10)
+            ax.add_patch(apo)
+            ax.text(*apoposdisp, 'A',transform=ax.transAxes,color='k',ha='center',va='center',size=4,zorder=10) 
+
+
+    #add a dot for the spacecraft location
+    if show_maven:
+        mavenpos=spice.spkezr('MAVEN',myet,'MAVEN_MSO','NONE','MARS')[0][:3]/3395
+        mavenposproj=np.array([np.dot(camera_right,mavenpos),np.dot(camera_up,mavenpos)])
+        mavenposdisp=(mavenposproj+extent)/extent/2
+        mavenvis=not (np.linalg.norm([mavenposproj])<1 and np.dot(camera_pos,mavenpos)<0)
+        if mavenvis:
+            maven = mpatches.Circle(mavenposdisp, 0.012, transform=ax.transAxes, fc=orbitcolor,lw=0,zorder=11)
+            ax.add_patch(maven)
+            ax.text(*mavenposdisp, 'M', transform=ax.transAxes, color='k', ha='center', va='center_baseline', size=4,zorder=11) 
+
+    #suppress all whitespace
+    plt.subplots_adjust(top = 1, bottom = 0, right = 1, left = 0, hspace = 0, wspace = 0)
+    plt.margins(0,0)
+    ax.set_axis_off()
+    ax.xaxis.set_major_locator(plt.NullLocator())
+    ax.yaxis.set_major_locator(plt.NullLocator()) 
+    
+    fig.canvas.draw()
+    
+    rgb_array=fig2rgb_array(fig)
+    
+    if show==False:
+        plt.close()
+    
+    return_coords={'extent':extent, 'scale':'3395 km', 'camera_pos':camera_pos, 'camera_pos_norm':camera_pos_norm, 'camera_up':camera_up, 'camera_right':camera_right,'orbit_coords':poslist}
+    
+    return rgb_array, return_coords
+
+    
+def maven_orbit_summary(time, show_maven=False):
+    fig,ax=plt.subplots(1,1,dpi=400,figsize=(5,5))
+    ax.set_xlim(0,2)
+    ax.set_ylim(0,2)
+    ax.set_aspect(1)
+
+    #make background black
+    ax.imshow(np.zeros([2,2,3]),extent=(0,1,0,1),transform=ax.transAxes)
+    
+    xview, orbinfo=maven_orbit_image(time,camera_pos=[1,0,0], camera_up=[0,0,1],show=False,show_maven=show_maven)
+    ax.imshow(xview,extent=(0,1,0,1))
+    yview, orbinfo=maven_orbit_image(time,camera_pos=[0,1,0], camera_up=[0,0,1],show=False,show_maven=show_maven)
+    ax.imshow(yview,extent=(1,2,0,1))
+    zview, orbinfo=maven_orbit_image(time,camera_pos=[0,0,1], camera_up=[-1,0,0],show=False,show_maven=show_maven)
+    ax.imshow(zview,extent=(0,1,1,2))
+    
+    #suppress all whitespace
+    plt.subplots_adjust(top = 1, bottom = 0, right = 1, left = 0, hspace = 0, wspace = 0)
+    plt.margins(0,0)
+    ax.set_axis_off()
+    ax.xaxis.set_major_locator(plt.NullLocator())
+    ax.yaxis.set_major_locator(plt.NullLocator())
+    
+    from .time import Ls
+    import spiceypy as spice
+    ax.text(0.02,0.98,time+"\nL"+r'$_\mathrm{s}$'+" = "+str(int(np.round(Ls(spice.str2et('2019 Jul 02')))))+r'$^\circ$',transform=ax.transAxes,color='w',ha='left',va='top')
