ee39fdc1f176d4870681f86a25b5e73312e1d087
Mike Chaffin <michael.s.chaffin@gmail.com>
2020-12-19 22:02:48 -0700

    Iuvs vm file sync (#6)
    * Added utility functions to sync data from the IUVS VM to the local machine.
    
    * fixed parentheses error, removed standard library requirements
    
    * Updates following Kyle's comments
    
    * support cases where password is not requied by rsync
    
    * updated call_rsync to use --progress flag to support MacOS usage of rsync from 2006.
    
    * changed timeouts so pexpect will fail if file transfer doesn't start quickly.
    
    * removed hard set of version for testing
    
    * Fix relative imports to be absolute
    
    Co-authored-by: Kyle <kdconey1@gmail.com>

--- setup.py
+++ setup.py
@@ -1,30 +1,28 @@
 import setuptools
 
 
-def setup_package():
-    setuptools.setup(
-        name='PyUVS',
-        version='0.1.0',
-        description='Utilities for Python 3 analysis of MAVEN/IUVS data.',
-        url='none yet',
-        author='Zachariah Milby',
-        packages=setuptools.find_packages(),
-        include_package_data=True,
-        python_requires='>=3.7',
-        install_requires=[
-            'astropy>=4.2',
-            'numpy>=1.10',
-            'matplotlib>=3.0.3',
-            'pdoc3>=0.9.1',
-            'julian>=0.14',
-            'sysrsync>=0.1.2',
-            'paramiko>=2.6.0',
-            'pytz>=2018.9',
-            'spiceypy>=2.2.0',
-        ],
-        dependency_links=['http://github.com/user/repo/tarball/master#egg=package-1.0']
-
-    )
-
-
-setup_package()
+setuptools.setup(
+    name='PyUVS',
+    version='0.1.0',
+    description='Utilities for Python 3 analysis of MAVEN/IUVS data.',
+    url='https://github.com/lasp/maven_iuvs',
+    author=('Zachariah Milby,'
+            ' Kyle Connour,'
+            ' Mike Chaffin,'
+            ' and the IUVS team'),
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    python_requires='>=3.7',
+    install_requires=[
+        'astropy>=4.2',
+        'numpy>=1.10',
+        'matplotlib>=3.0.3',
+        'pdoc3>=0.9.1',
+        'julian>=0.14',
+        'sysrsync>=0.1.2',
+        'paramiko>=2.6.0',
+        'pytz>=2018.9',
+        'spiceypy>=2.2.0',
+        'pexpect>=4.8.0'
+    ]
+)
