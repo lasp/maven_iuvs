59cd0fc2a160d53cdd2d0deb5c1d2eb77792cc4a
Zachariah Milby <zachariah.milby@lasp.colorado.edu>
2020-12-23 08:54:38 -0700

    Update to documentation

--- doc/make_documentation.py
+++ doc/make_documentation.py
@@ -1,5 +1,5 @@
 import os
 
 package_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..',))
-module_path = os.path.join(package_path, 'PyUVS')
+module_path = os.path.join(package_path, 'maven_iuvs')
 os.system('pdoc --html {} --force'.format(module_path))
