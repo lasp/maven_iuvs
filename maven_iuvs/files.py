26d2b70f25acd33afb8830ed0487a34be25d5550
Kyle <kdconey1@gmail.com>
2020-12-30 15:10:06 -0600

    Clean up file finding functions somewhat

--- maven_iuvs/files.py
+++ maven_iuvs/files.py
@@ -417,7 +417,18 @@ class SingleOrbitSequenceChannelL1bFiles(L1bFiles):
 #  if they can ever protect against appending, etc.
 
 
-def single_orbit_segment(path, orbit, channel='muv', sequence='apoapse'):
+# TODO: My functions here seem awfully clunky. It may be nice to define
+#  addition or summation as a method to combine multiple L1bFiles. But that too
+#  seems clunky since glob cannot handle an orbit range.
+# TODO: I iterate over an input list of orbits, but it would be nice to iterate
+#  over an input list of sequences and channels too so they're more general.
+# TODO: My helper functions may go elsewhere since they're not necessarily
+#  specific to the 3 major functions here.
+# TODO: The 3 major function names aren't very good...
+
+
+def single_orbit_segment(path, orbit, sequence='apoapse', channel='muv',
+                         recursive=False):
     """ Make a SingleOrbitSequenceChannelL1bFiles for files matching an input
     orbit, sequence, and channel.
 
@@ -427,10 +438,12 @@ def single_orbit_segment(path, orbit, channel='muv', sequence='apoapse'):
         The location where to start looking for files.
     orbit: int
         The orbit to get files from.
-    channel: str
-        The observing mode to get files from.
     sequence: str
         The observing sequence to get files from.
+    channel: str
+        The observing mode to get files from.
+    recursive: bool
+        Denote whether to look recursively in path. Default is False.
 
     Returns
     -------
@@ -438,15 +451,14 @@ def single_orbit_segment(path, orbit, channel='muv', sequence='apoapse'):
         A SingleOrbitSequenceChannelL1bFiles containing files from the
         requested orbit, sequence, and channel.
     """
-    pattern = f'**/*{sequence}-*{orbit}-*{channel}*'
+    pattern = make_recursive_glob_pattern(orbit, channel, sequence, recursive)
     return SingleOrbitSequenceChannelL1bFiles(path, pattern)
 
 
 # TODO: allow multiple paths so user could specify files in multiple dirs
 #     : like, if they want 3495--3510.
-# TODO: Allow for multiple channels and multiple sequences via '*'. Right now
-#     : it'll error since '**' is special.
-def orbital_segment(path, orbits, sequence='apoapse', channel='muv'):
+def orbital_segment(path, orbits, sequence='apoapse', channel='muv',
+                    recursive=False):
     """ Make an L1bFiles for an input list of orbits.
 
     Parameters
@@ -456,44 +468,34 @@ def orbital_segment(path, orbits, sequence='apoapse', channel='muv'):
     orbits: list
         List of ints of orbits to get files from.
     sequence: str
-        The observing sequence. Default is 'apoapse'.
+        The observing sequence. Can be '*'. Default is 'apoapse'.
     channel: str
-        The observing channel. Default is 'muv'.
+        The observing channel. Can be '*'. Default is 'muv'.
+    recursive: bool
+        Denote whether to look recursively in path. Default is False.
 
     Returns
     -------
-    files: L1bFiles
+    files: list
         An L1bFiles of all files at the input orbits.
     """
-    # TODO: this entire function an unreadable mess... fix
-    orbits = [str(orbit).zfill(5) for orbit in orbits]
-    patterns = [f'**/*{sequence}-*{orbit}-*{channel}*' for orbit in orbits]
+    orbits = orbits_to_strings(orbits)
+    patterns = make_patterns_from_orbits(orbits, sequence, channel, recursive)
 
-    l1b_files = []
-    for counter, pattern in enumerate(patterns):
+    # TODO: abstract this
+    single_orbit_files = []
+    for pattern in patterns:
         try:
             file = L1bFiles(path, pattern)
-            l1b_files.append(file)
+            single_orbit_files.append(file)
         except ValueError:
             continue
 
-    if len(l1b_files) == 0:
-        raise ValueError('There are no files for any of the input orbits.')
-    elif len(l1b_files) == 1:
-        return l1b_files
-    else:
-        for counter, files in enumerate(l1b_files):
-            if counter == 0:
-                file = files
-            else:
-                for j in range(len(files.absolute_paths)):
-                    file.absolute_paths.append(files.absolute_paths[j])
-                    file.filenames.append(files.filenames[j])
-    return file
+    return combine_multiple_l1b_files(single_orbit_files)
 
 
 def orbit_range_segment(path, orbit_start, orbit_end, sequence='apoapse',
-                        channel='muv'):
+                        channel='muv', recursive=False):
     """ Make an L1bFiles for all orbits between two endpoints.
 
     Parameters
@@ -505,9 +507,11 @@ def orbit_range_segment(path, orbit_start, orbit_end, sequence='apoapse',
     orbit_end: int
         The ending orbit to get files from.
     sequence: str
-        The observing sequence. Default is 'apoapse'.
+        The observing sequence. Can be '*'. Default is 'apoapse'.
     channel: str
-        The observing channel. Default is 'muv'.
+        The observing channel. Can be '*'. Default is 'muv'.
+    recursive: bool
+        Denote whether to look recursively in path. Default is False.
 
     Returns
     -------
@@ -515,4 +519,49 @@ def orbit_range_segment(path, orbit_start, orbit_end, sequence='apoapse',
         An L1bFiles of all files within the input orbital range.
     """
     orbits = list(range(orbit_start, orbit_end + 1))
-    return orbital_segment(path, orbits, sequence=sequence, channel=channel)
+    return orbital_segment(path, orbits, sequence=sequence, channel=channel,
+                           recursive=recursive)
+
+
+# TODO: Add try statements so the user doesn't get cryptic errors.
+def make_recursive_glob_pattern(orbit, sequence, channel, recursive):
+    pattern = make_single_segment_glob_pattern(orbit, sequence, channel)
+    if recursive:
+        pattern = prepend_recursive_glob_pattern(pattern)
+    return pattern
+
+
+def make_single_segment_glob_pattern(orbit, sequence, channel):
+    pattern = f'*{sequence}-*{orbit}-{channel}*'
+    return remove_recursive_glob_pattern(pattern)
+
+
+def remove_recursive_glob_pattern(pattern):
+    return pattern.replace('**', '*')
+
+
+def prepend_recursive_glob_pattern(pattern):
+    return f'**/{pattern}'
+
+
+def orbit_to_string(orbit):
+    return str(orbit).zfill(5)
+
+
+def orbits_to_strings(orbits):
+    return [orbit_to_string(orbit) for orbit in orbits]
+
+
+def make_patterns_from_orbits(orbits, sequence, channel, recursive):
+    return [make_recursive_glob_pattern(orbit, sequence, channel, recursive)
+            for orbit in orbits]
+
+
+def combine_multiple_l1b_files(l1b_files):
+    for counter, files in enumerate(l1b_files):
+        if counter == 0:
+            continue
+        for f in range(len(files.filenames)):
+            l1b_files[0].filenames.append(files.filenames[f])
+            l1b_files[0].absolute_paths.append(files.absolute_paths[f])
+    return l1b_files[0]
